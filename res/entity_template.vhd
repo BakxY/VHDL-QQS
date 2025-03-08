644bba5b6402e8eb0a62d8bf9ad795472168cbb1
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-08 16:20:51 +0100

    Added template file for new entities

---
+++ res/entity_template.vhd
@@ -0,0 +1,36 @@
+----------------------------------------------------------------------
+-- @file    ENTITY_NAME.vhd
+-- @brief   Entity implementation for ENTITY_NAME
+-- @date    DATE_CREATED
+-- @version v1.0.0
+-- @author  AUTHORS
+----------------------------------------------------------------------
+
+------ LIBRARY & USE STATEMENTS ------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+--------- ENTITY DECLARATION ---------
+entity ENTITY_NAME is
+    generic (
+        --! Define generic properties here
+    );
+    port (
+        --! Define inputs/outputs here
+    );
+end ENTITY_NAME;
+
+------ Architecture Declaration ------
+architecture rtl of ENTITY_NAME is
+    ------------ COMPONENTS --------------
+    --! Add component definitions here
+
+    -------------- SIGNALS ---------------
+    --! Add internal signals here
+
+    ------------- CONSTANTS --------------
+    --! Add constants here
+begin
+    --! Implement your entity here
+end rtl;
