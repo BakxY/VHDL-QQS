43cdfe8f4e6d52ffc3569d034f8d12ad87a8aede
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-18 21:53:27 +0100

    Implemented command to run analysis and elaboration of project

--- src/lib/CompileCommand.ts
+++ src/lib/CompileCommand.ts
@@ -50,3 +50,49 @@ export function compileQuartusProject(context: vscode.ExtensionContext, currentP
     vscode.window.showInformationMessage('Started compilation in terminal!');
     outputChannel.append('Started compilation in terminal!');
 }
+
+/**
+ * @brief Runs all support function to analyse and elaborate a quartus project and start a terminal with running compilation
+ * 
+ * @param context Context from where the command was ran
+ * @param currentProjectPath Path to the currently selected project
+ * @param pathToQuartus Path to the users quartus installation, where the binaries reside
+ */
+export function analyseQuartusProject(context: vscode.ExtensionContext, currentProjectPath: string, quartusBinPath: string): void {
+    const totalProjectPath = path.join(pathUtils.getWorkspacePath()!, currentProjectPath);
+    const totalQuartusBinPath = path.join(quartusBinPath, 'quartus_sh');
+    const totalScriptPath = path.join(context.extensionPath, 'res', 'analysis.tcl');
+
+    const scriptCmdArgs = '"' + totalProjectPath + '"';
+
+    const scriptCmd = '"' + totalQuartusBinPath + '" -t "' + totalScriptPath + '" ' + scriptCmdArgs;
+
+    // Get all active terminals opened in editor
+    const openTerminals = vscode.window.terminals;
+    let quartusCompileShell: vscode.Terminal | undefined = undefined;
+
+    // Filter for quartus terminal
+    for (let index = 0; index < openTerminals.length; index++) {
+        if (openTerminals[index].name === 'Quartus Compilation') {
+            quartusCompileShell = openTerminals[index];
+        }
+    }
+
+    // Check if a quartus shell was found
+    if (!quartusCompileShell) {
+        switch (process.platform) {
+            case 'win32':
+                quartusCompileShell = vscode.window.createTerminal('Quartus Compilation', PATH_TO_CMD);
+                break;
+            default:
+                quartusCompileShell = vscode.window.createTerminal('Quartus Compilation');
+        }
+    }
+
+    quartusCompileShell.show();
+    quartusCompileShell.sendText(scriptCmd);
+
+    console.log('Started analysis and elaboration in terminal!');
+    vscode.window.showInformationMessage('Started analysis and elaboration in terminal!');
+    outputChannel.append('Started analysis and elaboration in terminal!');
+}
