7bb4f015a0dcfd4a57f6d0ec890dc4dcb85ffbaf
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-21 19:58:32 +0100

    Removed all unused imports from command files

--- src/commands/openProgrammerActiveProject.ts
+++ src/commands/openProgrammerActiveProject.ts
@@ -4,18 +4,9 @@ import * as fs from 'fs';
 import * as cp from 'child_process';
 
 // Import custom libs
-import * as entityUtils from './../lib/EntityUtils';
-import * as testbenchCommands from './../lib/TestbenchCommand';
-import * as tomlUtils from './../lib/TomlUtils';
-import * as quartus from './../lib/QuartusUtils';
-import * as questa from './../lib/QuestaUtils';
-import * as compileCommands from './../lib/CompileCommand';
-import * as testCommands from './../lib/TestCommands';
-import * as statusBarCreator from './../lib/StatusBarUtils';
 import * as pathUtils from './../lib/PathUtils';
-import * as vhdlLang from './../lib/VhdlLang';
 
-import { outputChannel, quartusProjectFilesView, quartusProjectPropertiesView, currentQuestaProjectDisplay, runQuestaTestsButton, currentQuartusProjectDisplay, currentTopLevelDisplay } from '../extension';
+import { outputChannel } from '../extension';
 
 export function getCommand(context: vscode.ExtensionContext) {
     return vscode.commands.registerCommand('vhdl-qqs.openProgrammerActiveProject', async () => {
--- src/commands/selectQuestaProject.ts
+++ src/commands/selectQuestaProject.ts
@@ -1,21 +1,10 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as fs from 'fs';
-import * as cp from 'child_process';
 
 // Import custom libs
-import * as entityUtils from './../lib/EntityUtils';
-import * as testbenchCommands from './../lib/TestbenchCommand';
-import * as tomlUtils from './../lib/TomlUtils';
-import * as quartus from './../lib/QuartusUtils';
 import * as questa from './../lib/QuestaUtils';
-import * as compileCommands from './../lib/CompileCommand';
-import * as testCommands from './../lib/TestCommands';
-import * as statusBarCreator from './../lib/StatusBarUtils';
-import * as pathUtils from './../lib/PathUtils';
-import * as vhdlLang from './../lib/VhdlLang';
 
-import { outputChannel, quartusProjectFilesView, quartusProjectPropertiesView, currentQuestaProjectDisplay, runQuestaTestsButton, currentQuartusProjectDisplay, currentTopLevelDisplay } from '../extension';
+import { outputChannel, currentQuestaProjectDisplay } from '../extension';
 
 export function getCommand(context: vscode.ExtensionContext) {
     return vscode.commands.registerCommand('vhdl-qqs.selectQuestaProject', async () => {
