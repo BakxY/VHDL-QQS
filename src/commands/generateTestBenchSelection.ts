fb8534d1bc590bc13c34e2813357a527ae44372e
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-21 19:18:49 +0100

    Moved testbench commands to external files

---
+++ src/commands/generateTestBenchSelection.ts
@@ -0,0 +1,27 @@
+import * as vscode from 'vscode';
+import * as entityUtils from './../lib/EntityUtils';
+import * as testbenchCommands from './../lib/TestbenchCommand';
+
+import { outputChannel } from '../extension';
+
+export function getCommand(context: vscode.ExtensionContext) {
+    return vscode.commands.registerCommand('vhdl-qqs.generateTestBenchSelection', () => {
+        const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+    
+        // Check if editor is opened
+        if (editor === undefined) {
+            vscode.window.showErrorMessage('No editor opened!');
+            console.error('No editor opened!');
+            outputChannel.appendLine('No editor opened!');
+            return;
+        }
+    
+        console.log('Found current open file "' + editor.document.fileName + '"');
+        outputChannel.appendLine('Found current open file "' + editor.document.fileName + '"');
+    
+        const selectedExpression: string | null = entityUtils.getSelectedExpression(editor);
+        if (selectedExpression === null) { return; }
+    
+        testbenchCommands.createNewTestbench(context, selectedExpression, editor.document.fileName);
+    });
+} 
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as cp from 'child_process';
+
+// Import custom libs
 import * as entityUtils from './lib/EntityUtils';
 import * as testbenchCommands from './lib/TestbenchCommand';
 import * as tomlUtils from './lib/TomlUtils';
@@ -12,6 +14,10 @@ import * as testCommands from './lib/TestCommands';
 import * as statusBarCreator from './lib/StatusBarUtils';
 import * as pathUtils from './lib/PathUtils';
 import * as vhdlLang from './lib/VhdlLang';
+
+// Import command creators
+import * as generateTestBenchSelection from './commands/generateTestBenchSelection';
+import * as generateTestBenchExplorer from './commands/generateTestBenchExplorer';
 
 export let outputChannel: vscode.OutputChannel;
 
@@ -31,90 +37,13 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 	 * @brief Command generates a new testbench for a entity. This commands uses the user selected expression as the entity to generate the testbench for.
 	 * @author BakxY
 	 */
-	var disposable = vscode.commands.registerCommand('vhdl-qqs.generateTestBenchSelection', () => {
-		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
-
-		// Check if editor is opened
-		if (editor === undefined) {
-			vscode.window.showErrorMessage('No editor opened!');
-			console.error('No editor opened!');
-			outputChannel.appendLine('No editor opened!');
-			return;
-		}
-
-		console.log('Found current open file "' + editor.document.fileName + '"');
-		outputChannel.appendLine('Found current open file "' + editor.document.fileName + '"');
-
-		const selectedExpression: string | null = entityUtils.getSelectedExpression(editor);
-		if (selectedExpression === null) { return; }
-
-		testbenchCommands.createNewTestbench(context, selectedExpression, editor.document.fileName);
-	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(generateTestBenchSelection.getCommand(context));
 
 	/**
 	 * @brief Command generates a new testbench for a entity. This commands uses the toml file to get available entities and present the user with a menu selection.
 	 * @author BakxY
 	 */
-	var disposable = vscode.commands.registerCommand('vhdl-qqs.generateTestBenchExplorer', async () => {
-		// Get toml file path set in vs code setting
-		const pathToToml = pathUtils.getTomlLocalPath();
-		if (pathToToml === null) { return; }
-
-		// Get all entities listed in toml file
-		const allEntities = tomlUtils.getAllEntities(pathUtils.getWorkspacePath()!, pathToToml);
-		if (allEntities === null) { return; }
-
-		const allEntityNames: string[] = [];
-
-		// Remove file extensions
-		for (let entity = 0; entity < allEntities.length; entity++) {
-			allEntityNames[entity] = path.basename(allEntities[entity]).replace(path.extname(allEntities[entity]), '');
-		}
-
-		// Ask user to pick a entity
-		const selectedEntity: string | undefined = await vscode.window.showQuickPick(allEntityNames.sort(), { title: 'Select a entity to create a testbench' });
-		if (selectedEntity === undefined) { return; }
-
-		// Check if a testbench was selected to create a testbench
-		if (selectedEntity.endsWith('_tb')) {
-			vscode.window.showErrorMessage('Can\'t create a testbench of a testbench!');
-			console.error('Can\'t create a testbench of a testbench!');
-			outputChannel.appendLine('Can\'t create a testbench of a testbench!');
-			return;
-		}
-
-		// Check if testbench for selected entity already exists
-		if (allEntities.includes(selectedEntity + '_tb')) {
-			vscode.window.showErrorMessage('The testbench for this entity already exists!');
-			console.error('The testbench for this entity already exists!');
-			outputChannel.appendLine('The testbench for this entity already exists!');
-			return;
-		}
-
-		let pathToEntityFile: string = '';
-
-		// Check if selected entity has a entity file
-		for (const entity in allEntities) {
-			if (allEntities[entity].endsWith(selectedEntity + '.vhd')) {
-				pathToEntityFile = allEntities[entity];
-				console.log('Found file associated with selected entity at "' + allEntities[entity] + '"');
-				outputChannel.appendLine('Found file associated with selected entity at "' + allEntities[entity] + '"');
-				break;
-			}
-		}
-
-		// Trow error if no file was found
-		if (pathToEntityFile === '') {
-			vscode.window.showErrorMessage('Selected expression is not defined as a entity in your project!');
-			console.error('Selected expression is not defined as a entity in your project!');
-			outputChannel.appendLine('Selected expression is not defined as a entity in your project!');
-			return;
-		}
-
-		testbenchCommands.createNewTestbench(context, selectedEntity, pathToEntityFile);
-	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(generateTestBenchExplorer.getCommand(context));
 
 	/**
 	 * @brief Command sets the currently selected project for the current workspace.
