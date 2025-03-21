68edb58aef7b58178b2a053c375293f619991e89
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-21 19:42:20 +0100

    Moved project file commands to external files

--- src/extension.ts
+++ src/extension.ts
@@ -25,6 +25,8 @@ import * as cleanCompileFiles from './commands/cleanCompileFiles';
 import * as openProgrammerActiveProject from './commands/openProgrammerActiveProject';
 import * as openRtlViewerActiveProject from './commands/openRtlViewerActiveProject';
 import * as changeTopLevel from './commands/changeTopLevel';
+import * as addFileToProjectContext from './commands/addFileToProjectContext'
+import * as removeFileFromProjectContext from './commands/removeFileFromProjectContext'
 
 export let outputChannel: vscode.OutputChannel;
 export let quartusProjectFilesView: quartus.QuartusProjectFileTreeDataProvider;
@@ -104,85 +106,13 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 	 * @brief Command used in context menu to add a file to the current project.
 	 * @author BakxY
 	 */
-	var disposable = vscode.commands.registerCommand('vhdl-qqs.addFileToProjectContext', async (uri: vscode.Uri) => {
-		const filePath: string = path.normalize(uri.fsPath);
-		if (!['.vhd', '.v'].includes(path.extname(filePath))) { return; }
-
-		// Get currently active project
-		const activeProject: string | null = await pathUtils.getCurrentQuartusProject(context);
-		if (activeProject === null) { return; }
-
-		// Get  quartus install bin path
-		const quartusPath: string | null = await pathUtils.getQuartusBinPath();
-		if (quartusPath === null) { return; }
-
-		const relativePath = path.relative(path.dirname(path.join(pathUtils.getWorkspacePath()!, activeProject)), filePath).replaceAll('\\', '/');
-
-		switch (path.extname(filePath)) {
-			case '.vhd':
-				const allVhdlFiles = quartus.getProjectVhdlSourceFiles(context, activeProject, quartusPath);
-				if (allVhdlFiles.includes(relativePath)) {
-					vscode.window.showInformationMessage('File is already part of active project!');
-					return;
-				}
-				quartus.addVhdlFileToProject(context, activeProject, quartusPath, relativePath);
-				break;
-
-			case '.v':
-				const allVerilogFiles = quartus.getProjectVerilogSourceFiles(context, activeProject, quartusPath);
-				if (allVerilogFiles.includes(relativePath)) {
-					vscode.window.showInformationMessage('File is already part of active project!');
-					return;
-				}
-				quartus.addVerilogFileToProject(context, activeProject, quartusPath, relativePath);
-				break;
-		}
-		quartusProjectFilesView.updateData(context, activeProject, quartusPath);
-		vscode.window.showInformationMessage('Added file to active project!');
-	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(addFileToProjectContext.getCommand(context));
 
 	/**
 	 * @brief Command used in context menu to remove a file from the current project.
 	 * @author BakxY
 	 */
-	var disposable = vscode.commands.registerCommand('vhdl-qqs.removeFileFromProjectContext', async (uri: vscode.Uri) => {
-		const filePath: string = path.normalize(uri.fsPath);
-		if (!['.vhd', '.v'].includes(path.extname(filePath))) { return; }
-
-		// Get currently active project
-		const activeProject: string | null = await pathUtils.getCurrentQuartusProject(context);
-		if (activeProject === null) { return; }
-
-		// Get  quartus install bin path
-		const quartusPath: string | null = await pathUtils.getQuartusBinPath();
-		if (quartusPath === null) { return; }
-
-		const relativePath = path.relative(path.dirname(path.join(pathUtils.getWorkspacePath()!, activeProject)), filePath).replaceAll('\\', '/');
-
-		switch (path.extname(filePath)) {
-			case '.vhd':
-				const allVhdlFiles = quartus.getProjectVhdlSourceFiles(context, activeProject, quartusPath);
-				if (!allVhdlFiles.includes(relativePath)) {
-					vscode.window.showInformationMessage('Was\'t part of project!');
-					return;
-				}
-				quartus.removeVhdlFileToProject(context, activeProject, quartusPath, relativePath);
-				break;
-
-			case '.v':
-				const allVerilogFiles = quartus.getProjectVerilogSourceFiles(context, activeProject, quartusPath);
-				if (!allVerilogFiles.includes(relativePath)) {
-					vscode.window.showInformationMessage('Was\'t part of project!');
-					return;
-				}
-				quartus.removeVerilogFileToProject(context, activeProject, quartusPath, relativePath);
-				break;
-		}
-		quartusProjectFilesView.updateData(context, activeProject, quartusPath);
-		vscode.window.showInformationMessage('Removed file from active project!');
-	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(removeFileFromProjectContext.getCommand(context));
 
 	/**
 	 * @brief Command used to remove a file from the current project by a user menu selection.
